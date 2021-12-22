0a8fad62a29f0adfdae2c2c78f8829c3eeeea221
Christer Lingard <lingard.christer@gmail.com>
2021-12-22 12:57:26 +0100

    add tests & fix skipUntilChanged operator

--- src/operators.ts
+++ src/operators.ts
@@ -1,17 +1,20 @@
 import { pipe } from 'fp-ts/function'
 import { Eq } from 'fp-ts/Eq'
-import { skipWhile, Source, tap } from 'wonka'
+import * as O from 'fp-ts/Option'
+import { filter, map, scan, Source } from 'wonka'
 
 export const skipUntilChanged =
   <A>(eq: Eq<A>) =>
-  (s: Source<A>) => {
-    let prev: A
-
-    return pipe(
+  (s: Source<A>) =>
+    pipe(
       s,
-      skipWhile((value) => eq.equals(value, prev)),
-      tap((value) => {
-        prev = value
-      })
+      scan<A, [O.Option<A>, boolean]>(
+        ([prev], value) => [
+          O.some(value),
+          O.getEq(eq).equals(prev, O.fromNullable(value))
+        ],
+        [O.none, false]
+      ),
+      filter(([, eq]) => !eq),
+      map(([value]) => O.toNullable(value))
     )
-  }
---
+++ tests/operators.ts
@@ -0,0 +1,14 @@
+import assert from 'assert'
+import { eqStrict } from 'fp-ts/Eq'
+import { pipe } from 'fp-ts/function'
+import { fromArray, toArray } from 'wonka'
+import { skipUntilChanged } from '../src/operators'
+
+describe('skipUntilChanged', () => {
+  it('skips until value is changed', () => {
+    const input = [1, 1, 2, 3]
+    const events = pipe(fromArray(input), skipUntilChanged(eqStrict), toArray)
+
+    assert.deepStrictEqual(events, [1, 2, 3])
+  })
+})
