ee76d514a6fcda2ded662c47c66b0b6d1afabb49
Christer Lingard <lingard.christer@gmail.com>
2021-12-22 13:06:23 +0100

    fix skipUntilChanged return type

--- src/operators.ts
+++ src/operators.ts
@@ -5,16 +5,16 @@ import { filter, map, scan, Source } from 'wonka'
 
 export const skipUntilChanged =
   <A>(eq: Eq<A>) =>
-  (s: Source<A>) =>
+  (s: Source<A>): Source<A> =>
     pipe(
       s,
       scan<A, [O.Option<A>, boolean]>(
         ([prev], value) => [
           O.some(value),
-          O.getEq(eq).equals(prev, O.fromNullable(value))
+          O.getEq(eq).equals(prev, O.some(value))
         ],
         [O.none, false]
       ),
       filter(([, eq]) => !eq),
-      map(([value]) => O.toNullable(value))
+      map(([value]) => O.toNullable(value) as A)
     )
